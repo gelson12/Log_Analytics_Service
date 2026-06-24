b9f9c4308dd785c209cf3a8661d5a6d581796687
gelson12 <gelson_m@hotmail.com>
2026-06-24 11:37:02 +0100

    fix: ecs variables.tf invalid semicolon syntax

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -1,15 +1,64 @@
-variable "name"                    { type = string }
-variable "environment"             { type = string }
-variable "aws_region"              { type = string }
-variable "vpc_id"                  { type = string }
-variable "public_subnet_ids"       { type = list(string) }
-variable "private_subnet_ids"      { type = list(string) }
-variable "ecr_image_uri"           { type = string }
-variable "task_execution_role_arn" { type = string }
-variable "task_role_arn"           { type = string }
-variable "container_port"          { type = number; default = 8000 }
-variable "cpu"                     { type = number; default = 256 }
-variable "memory"                  { type = number; default = 512 }
-variable "desired_count"           { type = number; default = 2 }
-variable "log_bucket"              { type = string }
-variable "git_sha"                 { type = string; default = "unknown" }
+variable "name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "ecr_image_uri" {
+  type = string
+}
+
+variable "task_execution_role_arn" {
+  type = string
+}
+
+variable "task_role_arn" {
+  type = string
+}
+
+variable "container_port" {
+  type    = number
+  default = 8000
+}
+
+variable "cpu" {
+  type    = number
+  default = 256
+}
+
+variable "memory" {
+  type    = number
+  default = 512
+}
+
+variable "desired_count" {
+  type    = number
+  default = 2
+}
+
+variable "log_bucket" {
+  type = string
+}
+
+variable "git_sha" {
+  type    = string
+  default = "unknown"
+}
